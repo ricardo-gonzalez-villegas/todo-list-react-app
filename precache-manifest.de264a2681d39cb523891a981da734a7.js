21524bcb27b27188bd35c0431ca19ed8baa46e7d
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-05-05 20:31:31 -0400

    Updates

---
+++ precache-manifest.de264a2681d39cb523891a981da734a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d707adf7dd20fffad956c4571052f8b6",
+    "url": "/todo-list-react-app/index.html"
+  },
+  {
+    "revision": "db90af563bc951201aff",
+    "url": "/todo-list-react-app/static/css/main.dfd10190.chunk.css"
+  },
+  {
+    "revision": "3903bd759d8f6718c238",
+    "url": "/todo-list-react-app/static/js/2.b3e9b2d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react-app/static/js/2.b3e9b2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db90af563bc951201aff",
+    "url": "/todo-list-react-app/static/js/main.6b5a7e87.chunk.js"
+  },
+  {
+    "revision": "6f763bd7ac0d24e457d8",
+    "url": "/todo-list-react-app/static/js/runtime-main.7943f196.js"
+  }
+]);
